76d9238b3315947ed5588b99aec9eb9c77178571
Srishti Yadav <srishtiy1301@outlook.com>
2025-07-31 17:06:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from translator import translate_text
 
@@ -13,4 +14,6 @@ def index():
     return render_template("index.html", translated_text=translated_text)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
